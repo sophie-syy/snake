95dfd44f2cd89e3a58c49bcf24d929c7b80d9129
TheAnonymeBoy <c4nameless@gmail.com>
2025-12-08 12:14:23 +0000

    Add files via upload

---
+++ code/src/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "map.h"
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Usage: %s Donner le fichier map en commentaire \n", argv[0]);
+        return 1;
+    }
+
+    Map *map = load_map(argv[1]);
+    if (map == NULL) {
+        return 1;
+    }
+
+    printf("Map chargée (%dx%d) :\n", map->width, map->height);
+
+    print_map(map);
+    printf("case 4,6 = %c\n",map->data[4][6]);
+//free_map(map);
+    return 0;
+}
